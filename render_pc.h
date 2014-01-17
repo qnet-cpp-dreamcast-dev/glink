33a4047fcbd030b5fa8dbb5e5dd8b04e0f26a989
graybriggs <grahambriggs@gmail.com>
2014-01-17 19:30:24 +0000

    sprite struct

--- render_pc.h
+++ render_pc.h
@@ -6,7 +6,18 @@
 #include "SDL/SDL.h"
 #include "glink_types.h"
 
-extern void pc_render_sprite(const int, const int);
+
+typedef _glink_sprite {
+
+	const char* name;		// sprite name or id
+	SDL_Surface* surface;	// surface of the sprite
+	glink_rect box;  		// bounding box of the sprite
+	unsigned char alpha;	// alpha value of the sprite
+
+} glink_sprite;
+
+
+extern void pc_render_sprite(const glink_sprite, const glink_rect, const int, const int);
 extern void pc_render_rect(const int, const int, const int, const int, const unsigned int);
 extern void pc_render_rect_r(const glink_rect, const unsigned int);
 extern void pc_blank_video_buffer();
