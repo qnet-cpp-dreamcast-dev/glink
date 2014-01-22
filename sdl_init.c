7a57d47fa7905435c6506ce189639651cba202f6
graybriggs <grahambriggs@gmail.com>
2014-01-22 17:47:53 +0000

    removed old code

--- sdl_init.c
+++ sdl_init.c
@@ -7,20 +7,12 @@ SDL_Surface* screen = NULL;
 // keystates - from *_input.h
 unsigned char keystates[0xFF];
 
-//unsigned char* keystates = NULL;
 
 void glink_pc_setup(unsigned int w, unsigned int h) {
 
 	screen = SDL_SetVideoMode(w, h, 32, SDL_SWSURFACE);
 
 	memset(keystates, 0x00, sizeof(unsigned char) * 0xFF);
-	/*
-	for (int i = 0x00; i < 0xFF; ++i) {
-		keystates[i] = 0x00;
-	}
-	*/
-
-	//keystates = (unsigned char*)malloc(0xFF * sizeof(unsigned char));
 }
 
 void glink_pc_shutdown() {
